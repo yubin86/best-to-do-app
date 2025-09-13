b0ee536f05cf1afccda1ac02c3d10cc641693812
yubin86 <yubinlee8645@gmail.com>
2025-09-13 12:04:09 -0700

    4th commit - implementing cors on server

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import todos from "./routes/todos.js";
+import cors from "cors"
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req,res)=> res.send("welcome"));
 
 app.use("/api/todos", todos);
